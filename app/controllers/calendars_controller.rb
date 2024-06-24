92eceb37857bfa18711afd5ed0f0bec029f9a3ec
minamieri <sunaga.eri.mail@gmail.com>
2024-06-25 00:38:05 +0900

    issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,14 +8,18 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
-    redirect_to action: :index
+    @plan = Plan.new(plan_params)
+    if @plan.save
+     redirect_to action: :index, notice: '予定が保存されました。'
+    else
+      render :new
+    end
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
